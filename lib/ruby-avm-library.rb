34ec07e60427bdd223ed90738e0b3435dd577d07
John Bintz <john@coswellproductions.com>
2011-03-03 17:36:12 -0500

    bundler gem

---
+++ lib/ruby-avm-library.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Avm
+    module Library
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby-avm-library/version.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Avm
+    module Library
+      VERSION = "0.0.1"
+    end
+  end
+end
